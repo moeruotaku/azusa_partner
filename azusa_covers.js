78e2780d6c05e8d0ff5354bc5474f9364995ac7e
moeruotaku <moeruotaku@gmail.com>
2025-04-14 09:27:56 +0800

    2025.04.14.39

--- azusa_covers.js
+++ azusa_covers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.04.11.86
+// @version     2025.04.14.39
 // @description azusa_covers
 // @author      moeruotaku
 // @license     MIT
@@ -3116,5 +3116,7 @@ const azusa_covers = {
   28566: "https://u2.dmhy.org/attachments/202504/20250405091338ede1b897b408bba2027aa9fcc1e63109.jpg.thumb.jpg",
   28589: "https://img.azusa.wiki/images/2025/04/08/1.jpg",
   28604: "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/3426820/header.jpg?t=1744210836",
-  28605: "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/3426830/header.jpg?t=1744210850"
+  28605: "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/3426830/header.jpg?t=1744210850",
+  28619: "//lain.bgm.tv/pic/cover/l/44/84/482406_T0Sqx.jpg",
+  28627: "//lain.bgm.tv/r/400/pic/cover/l/3d/22/41802_SqJXa.jpg"
 };
