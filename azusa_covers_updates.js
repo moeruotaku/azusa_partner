15c93f247145490c99b91b1c71b3e20b78348c61
moeruotaku <moeruotaku@gmail.com>
2025-05-23 10:13:14 +0800

    2025.05.23.42

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.05.23.35
+// @version     2025.05.23.42
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
--- wall.js
+++ wall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_wall
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.05.23.35
+// @version     2025.05.23.42
 // @description add wall to azusa
 // @author      moeruotaku
 // @license     MIT
