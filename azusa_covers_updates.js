8a756fc5d5b5ad585ba1f964fa66132c39ba568f
moeruotaku <moeruotaku@gmail.com>
2025-06-19 13:34:55 +0800

    2025.06.19.56

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.06.16.47
+// @version     2025.06.19.56
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,9 +12,7 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  29142: "https://minio.pting.club/lskypro/2025/06/09/6845b9ddaf12f.jpg",
-  29148: "//lain.bgm.tv/r/400/pic/cover/l/1f/da/525223_lsQz4.jpg",
-  29154: "https://lain.bgm.tv/r/400/pic/cover/l/b4/f9/468253_su6TA.jpg",
   29187: "https://taiwan-image.bookwalker.com.tw/product/214728/zoom_big_214728.jpg",
-  29188: "https://taiwan-image.bookwalker.com.tw/product/16429/16429_1.jpg"
+  29188: "https://taiwan-image.bookwalker.com.tw/product/16429/16429_1.jpg",
+  29199: "https://t.tutu.to/img/leiMH"
 };
