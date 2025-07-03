edd364f8d34f5c35a7d36a7658c3c592c99ef683
moeruotaku <moeruotaku@gmail.com>
2025-07-03 10:22:11 +0800

    2025.07.03.43

--- azusa_covers.js
+++ azusa_covers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.07.03.36
+// @version     2025.07.03.43
 // @description azusa_covers
 // @author      moeruotaku
 // @license     MIT
--- bgm_tags.js
+++ bgm_tags.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_tags
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.07.03.36
+// @version     2025.07.03.43
 // @description bgm_tags
 // @author      moeruotaku
 // @license     MIT
