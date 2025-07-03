1c95ac888479092fcfe068f97d03efe2f7b804d6
moeruotaku <moeruotaku@gmail.com>
2025-07-03 23:27:00 +0800

    2025.07.03.97

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.07.03.95
+// @version     2025.07.03.97
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -16,5 +16,6 @@ const azusa_covers = {
   29322: "https://img1.pixhost.to/images/6896/617359878_cover.jpg",
   29326: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1955830/header_schinese.jpg?t=1717159959",
   29327: "//lain.bgm.tv/pic/cover/l/63/a5/392295_AmibU.jpg",
-  29330: "https://img1.pixhost.to/images/6921/617849216_cover.jpg"
+  29330: "https://img1.pixhost.to/images/6921/617849216_cover.jpg",
+  29339: "https://res2.pubu.tw/docs/498577/99031/V-3Uif_xl.jpg"
 };
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.07.03.95
+// @version     2025.07.03.97
 // @description add bgm info to azusa
 // @author      moeruotaku
 // @license     MIT
