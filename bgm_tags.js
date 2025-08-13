9eff5650b3116f2861c7714c076e3a7a3f36f620
moeruotaku <moeruotaku@gmail.com>
2025-08-13 17:26:13 +0800

    2025.08.13.72

--- bgm_tags.js
+++ bgm_tags.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_tags
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.08.13.59
+// @version     2025.08.13.72
 // @description bgm_tags
 // @author      moeruotaku
 // @license     MIT
