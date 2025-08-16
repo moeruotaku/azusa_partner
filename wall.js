4b115f1f3c25a1c6427816796d3f2c81d8b7fa00
moeruotaku <moeruotaku@gmail.com>
2025-08-17 02:38:09 +0800

    2025.08.17.10

--- wall.js
+++ wall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_wall
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.08.16.97
+// @version     2025.08.17.10
 // @description add wall to azusa
 // @author      moeruotaku
 // @license     MIT
