0502c86f99319956ccf55ede5dd4bbbe179084b4
moeruotaku <moeruotaku@gmail.com>
2025-08-27 17:47:32 +0800

    2025.08.27.74

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.08.27.43
+// @version     2025.08.27.74
 // @description add bgm info to azusa
 // @author      moeruotaku
 // @license     MIT
