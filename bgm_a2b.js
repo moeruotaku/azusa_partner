aad7cd17a89f39625373cad68ff8acb8c97145ac
moeruotaku <moeruotaku@gmail.com>
2025-09-10 17:29:42 +0800

    2025.09.10.72

--- bgm_a2b.js
+++ bgm_a2b.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_a2b
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.09.10.36
+// @version     2025.09.10.72
 // @description bgm_a2b
 // @author      moeruotaku
 // @license     MIT
