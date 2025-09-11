a459459006c593e49cb2fc5bf20ba06d39cb0a74
moeruotaku <moeruotaku@gmail.com>
2025-09-11 17:26:30 +0800

    2025.09.11.72

--- bgm_scores_updates.js
+++ bgm_scores_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_scores_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.09.11.61
+// @version     2025.09.11.72
 // @description bgm_scores_updates
 // @author      moeruotaku
 // @license     MIT
