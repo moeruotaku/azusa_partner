cceefe9c15d0c8679524efde14c20746752b5d1e
moeruotaku <moeruotaku@gmail.com>
2025-10-07 00:29:13 +0800

    2025.10.07.02

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.05.59
+// @version     2025.10.07.02
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -16,5 +16,11 @@ const azusa_covers = {
   30228: "https://cdn.myanonamouse.net/tor/poster.php/bDahXF+PjDO8bQ,p1L2x9V9,sUi2duhEzTL5+74V6Q0wMTIzNDU2Nzg5QUJDREVGR0hJSktMTU44N9qmOjPfy3FR58Dkq2XIpKhcjA5aP6LDl6XnX0kVs32sOTRn5f1QmKifHaeJ3Lwm80fp3r3IR9YRU1zZzTA=",
   30240: "https://lain.bgm.tv/pic/cover/l/cd/ee/21767_unPBu.jpg",
   30253: "https://lain.bgm.tv/pic/cover/l/a9/4a/298102_WH9jV.jpg?_gl=1*p8pqr2*_ga*MjA1NzUyMjUwNC4xNzU0MDM4NTYz*_ga_1109JLGMHN*czE3NTk1NDE2MTYkbzE2JGcxJHQxNzU5NTQxNjE4JGo1OCRsMCRoMA..",
-  30268: "//lain.bgm.tv/pic/cover/l/a5/71/328029_0t22Z.jpg"
+  30268: "//lain.bgm.tv/pic/cover/l/a5/71/328029_0t22Z.jpg",
+  30270: "https://pic.pting.club/i/2025/10/06/68e2bafdea246.jpg",
+  30279: "https://p.sda1.dev/27/ea42d7a85d8c9bc59541794393c21d65/cover.jpg",
+  30282: "//lain.bgm.tv/r/400/pic/cover/l/c0/02/565693_wKaaz.jpg",
+  30289: "//lain.bgm.tv/r/400/pic/cover/l/2e/a7/392774_ia6BI.jpg",
+  30292: "//lain.bgm.tv/r/400/pic/cover/l/f4/dd/477276_nbuYa.jpg",
+  30296: "https://p.inari.site/guest/25-10/06/68e33242e8f50.jpg"
 };
--- bgm_covers_updates.js
+++ bgm_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.05.59
+// @version     2025.10.07.02
 // @description bgm_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -14,7 +14,9 @@
 const bgm_covers = {
   19289: "6a/44/19289_PtRrC.jpg",
   38542: "e4/71/38542_jYa0Z.jpg",
+  89134: "21/77/89134_YNojB.jpg",
   129557: "be/6f/129557_sW4rl.jpg",
+  141545: "24/40/141545_tp7ER.jpg",
   176089: "97/28/176089_ccCcc.jpg",
   184047: "48/f3/184047_29n5L.jpg",
   186792: "e5/50/186792_wbRws.jpg",
@@ -25,17 +27,26 @@ const bgm_covers = {
   248883: "83/f9/248883_nw7l0.jpg",
   262170: "69/4c/262170_L2ctt.jpg",
   265141: "d2/bf/265141_cHcly.jpg",
+  272611: "f9/28/272611_8ZtZK.jpg",
   273493: "47/9e/273493_ZMBD7.jpg",
+  273590: "e7/f4/273590_2151L.jpg",
+  279379: "c6/ab/279379_Bjc7O.jpg",
   289826: "b4/f5/289826_MyRaG.jpg",
+  291900: "5c/16/291900_AJcps.jpg",
   298144: "be/9b/298144_kvT9z.jpg",
   299985: "d8/6a/299985_e0ieO.jpg",
   305429: "a1/bd/305429_axzF3.jpg",
   318168: "bd/b4/318168_3dLDa.jpg",
+  320977: "da/2c/320977_6F1R6.jpg",
+  321869: "dd/0b/321869_JoWzG.jpg",
   324366: "40/5c/324366_J5AaM.jpg",
   333285: "1b/a3/333285_2ufJ4.jpg",
+  335456: "30/8c/335456_9Dm6F.jpg",
   336489: "ed/7f/336489_ybgjG.jpg",
-  337711: "0d/50/337711_xw2R4.jpg",
+  340182: "7c/aa/340182_5yyYK.jpg",
   349842: "da/4a/349842_u5ug7.jpg",
+  364561: "0b/30/364561_k51wD.jpg",
+  368650: "23/a2/368650_L2Zad.jpg",
   372262: "d5/80/372262_e9S0l.jpg",
   388039: "e1/cd/388039_DJ7GF.jpg",
   388852: "71/82/388852_hi6sC.jpg",
@@ -44,15 +55,24 @@ const bgm_covers = {
   393233: "55/da/393233_W033K.jpg",
   397777: "43/f8/397777_953G5.jpg",
   403992: "a9/6a/403992_F0483.jpg",
-  418009: "c9/aa/418009_VpGvq.jpg",
   424917: "0a/d6/424917_4gb4n.jpg",
+  425732: "25/25/425732_A78o8.jpg",
   427143: "97/2e/427143_QzLOZ.jpg",
   446065: "be/ae/446065_7TXsU.jpg",
   447981: "bc/67/447981_gGvuo.jpg",
+  466654: "9a/46/466654_suBzO.jpg",
+  475214: "94/7c/475214_j779i.jpg",
+  482879: "25/9b/482879_NMSna.jpg",
+  484353: "60/0e/484353_T6L53.jpg",
+  485035: "0a/05/485035_5rzw5.jpg",
   489490: "92/66/489490_9aZPS.jpg",
+  498301: "29/0d/498301_CNN11.jpg",
   505952: "d0/2f/505952_9Jcfc.jpg",
+  508124: "fc/d4/508124_Zi7YF.jpg",
   509947: "8f/03/509947_yEeWw.jpg",
   511746: "2d/a1/511746_1ZEaG.jpg",
   523498: "33/56/523498_7lxXX.jpg",
-  530454: "2c/b9/530454_406NP.jpg"
+  530454: "2c/b9/530454_406NP.jpg",
+  541044: "fc/55/541044_IDvvZ.jpg",
+  562302: "99/70/562302_Ff6Ff.jpg"
 };
