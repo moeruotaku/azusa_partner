087575ef2b0834b80ed0b3fea3cab3cba2c4b63b
moeruotaku <moeruotaku@gmail.com>
2025-10-15 17:14:04 +0800

    2025.10.15.71

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.15.36
+// @version     2025.10.15.71
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -16,5 +16,9 @@ const azusa_covers = {
   30370: "https://p.sda1.dev/27/cb515257526742ec7280dd56c16c03d8/image.png",
   30389: "https://p.sda1.dev/27/a4bcf2ea79839314c23e02f57995dc34/image.png",
   30393: "https://p.sda1.dev/27/bb18573a7bd6cfb6e79791b8a8481d8a/image.png",
-  30395: "https://p.sda1.dev/27/963f489b46cf8d2f93869dcb308d3ce6/image.png"
+  30395: "https://p.sda1.dev/27/963f489b46cf8d2f93869dcb308d3ce6/image.png",
+  30401: "https://p.sda1.dev/27/3f6f9c328a2bfb4b6d25a9239cf94e72/image.png",
+  30406: "https://lain.bgm.tv/r/400/pic/cover/l/3d/23/397615_r52Dt.jpg",
+  30409: "https://p.sda1.dev/27/8cb2d3c8bc4e9f9b6d3b71a7fa7762c7/telegram-cloud-photo-size-1-5143178583860775686-y.jpg",
+  30415: "https://lain.bgm.tv/r/400/pic/cover/l/36/01/466141_T3Zn6.jpg"
 };
