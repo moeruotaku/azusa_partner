b2cf2608e2fd4347fdac0a950abd2432f1e7233a
moeruotaku <moeruotaku@gmail.com>
2025-10-26 14:24:42 +0800

    2025.10.26.60

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.24.36
+// @version     2025.10.26.60
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,12 +12,12 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  30470: "https://lain.bgm.tv/r/400/pic/cover/l/c0/22/328104_rUt3F.jpg",
-  30476: "https://img.nnko.me/251008-02.jpg",
-  30481: "https://img.nnko.me/251008-08.jpg",
-  30488: "https://www.tongli.com.tw/ComicImages/Images/TC0365/TC0365001A/TC0365001A.jpg",
   30529: "https://lain.bgm.tv/r/400/pic/cover/l/8e/85/598961_XO0na.jpg",
   30542: "https://lain.bgm.tv/r/400/pic/cover/l/73/2e/574952_90Psw.jpg",
   30543: "https://p.sda1.dev/28/b401e5fdae96c7878c2655bf7406190f/cover.jpg",
-  30549: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp"
+  30549: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp",
+  30556: "https://photo.vclib.online/i/2025/10/24/ukvi6i.png",
+  30557: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp",
+  30561: "https://img.nnko.me/251024-05.jpeg",
+  30583: "https://img.nnko.me/251026-04.jpg"
 };
