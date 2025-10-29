d90c7c3423aebab69811aaecc5ddfe9b6351da1b
moeruotaku <moeruotaku@gmail.com>
2025-10-29 10:24:54 +0800

    2025.10.29.43

--- azusa_covers.js
+++ azusa_covers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.27.35
+// @version     2025.10.29.43
 // @description azusa_covers
 // @author      moeruotaku
 // @license     MIT
@@ -3514,5 +3514,8 @@ const azusa_covers = {
   30556: "https://photo.vclib.online/i/2025/10/24/ukvi6i.png",
   30557: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp",
   30561: "https://img.nnko.me/251024-05.jpeg",
-  30583: "https://img.nnko.me/251026-04.jpg"
+  30583: "https://img.nnko.me/251026-04.jpg",
+  30607: "https://img.nnko.me/251027-04.jpg",
+  30609: "https://photo.vclib.online/i/2025/10/27/w5269u.bmp",
+  30614: "https://pic.pting.club/i/2025/10/28/6900bd88181ff.png"
 };
--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.10.27.35
+// @version     2025.10.29.43
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,12 +12,14 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  30529: "https://lain.bgm.tv/r/400/pic/cover/l/8e/85/598961_XO0na.jpg",
   30542: "https://lain.bgm.tv/r/400/pic/cover/l/73/2e/574952_90Psw.jpg",
   30543: "https://p.sda1.dev/28/b401e5fdae96c7878c2655bf7406190f/cover.jpg",
   30549: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp",
   30556: "https://photo.vclib.online/i/2025/10/24/ukvi6i.png",
   30557: "https://photo.vclib.online/i/2025/10/23/z17hzu.bmp",
   30561: "https://img.nnko.me/251024-05.jpeg",
-  30583: "https://img.nnko.me/251026-04.jpg"
+  30583: "https://img.nnko.me/251026-04.jpg",
+  30607: "https://img.nnko.me/251027-04.jpg",
+  30609: "https://photo.vclib.online/i/2025/10/27/w5269u.bmp",
+  30614: "https://pic.pting.club/i/2025/10/28/6900bd88181ff.png"
 };
