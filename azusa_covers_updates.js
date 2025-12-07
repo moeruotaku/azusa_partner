27135e4d63336c0e361a8a5331e6e5a85991e0e6
moeruotaku <moeruotaku@gmail.com>
2025-12-07 21:04:55 +0800

    2025.12.07.87

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.12.05.42
+// @version     2025.12.07.87
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,12 +12,18 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  30836: "https://lain.bgm.tv/r/400/pic/cover/l/cb/42/433080_XETe9.jpg",
   30842: "https://lain.bgm.tv/r/400/pic/cover/l/56/e3/372975_v339t.jpg",
   30845: "https://lain.bgm.tv/r/400/pic/cover/l/67/df/560376_2e720.jpg",
   30850: "https://lain.bgm.tv/r/400/pic/cover/l/01/30/597970_7z9dp.jpg",
   30855: "https://lain.bgm.tv/pic/cover/l/de/38/495164_Vex1a.jpg",
   30858: "https://tongliebookpic.blob.core.windows.net/bookcover/3a8056b25d774beb5bc208de308f394d.jpg",
   30859: "https://tongliebookpic.blob.core.windows.net/bookcover/a579e556ce004ed45bba08de308f394d.jpg",
-  30861: "https://lain.bgm.tv/pic/cover/l/ac/c1/506840_RdBrM.jpg"
+  30861: "https://lain.bgm.tv/pic/cover/l/ac/c1/506840_RdBrM.jpg",
+  30873: "https://lain.bgm.tv/r/400/pic/cover/l/2d/29/103535_5fVWo.jpg",
+  30874: "https://lain.bgm.tv/r/400/pic/cover/l/50/4b/7088_jp.jpg",
+  30875: "https://lain.bgm.tv/r/400/pic/cover/l/bc/1c/183829_jp.jpg",
+  30882: "https://lain.bgm.tv/r/400/pic/cover/l/f1/cb/247383_eSsr0.jpg",
+  30884: "https://lain.bgm.tv/r/400/pic/cover/l/c1/49/187715_12H22.jpg",
+  30885: "https://lain.bgm.tv/r/400/pic/cover/l/f8/91/155587_YrELD.jpg",
+  30887: "https://res.lightnovel.fun/images/251201/8ff784e3cc3a2e1b7b71ad5e333f3c31.jpg?m=4sbaWAyF10ILTIJbm-eLsQ&amp;t=1765038597"
 };
--- bgm_covers_updates.js
+++ bgm_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2025.12.05.42
+// @version     2025.12.07.87
 // @description bgm_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -13,24 +13,35 @@
 
 const bgm_covers = {
   6784: "03/32/6784_aC6ah.jpg",
-  9344: "40/42/9344_jp.jpg",
   32818: "c6/a7/32818_REH3I.jpg",
-  122776: "bb/a1/122776_5hEUc.jpg",
-  146872: "33/4f/146872_C86z3.jpg",
-  163847: "ec/4a/163847_I1fmV.jpg",
+  121365: "95/20/121365_80u77.jpg",
+  132823: "fc/7d/132823_GILmw.jpg",
+  185741: "72/31/185741_oK9z4.jpg",
+  207478: "cc/29/207478_q60qo.jpg",
+  212037: "cd/2a/212037_2f8kE.jpg",
+  218561: "ed/25/218561_3Zz3B.jpg",
+  218701: "df/f1/218701_Ss9VJ.jpg",
+  221759: "8e/6f/221759_505o5.jpg",
   221946: "78/e1/221946_J30AA.jpg",
-  231030: "0d/5a/231030_w1BG1.jpg",
+  236546: "84/97/236546_9Jzv3.jpg",
   240256: "0b/46/240256_4wI91.jpg",
+  255324: "f6/0c/255324_8wP9o.jpg",
+  260215: "da/98/260215_cmD2V.jpg",
+  267399: "e4/87/267399_3vrqK.jpg",
+  272961: "80/22/272961_uE9Cs.jpg",
+  273878: "94/06/273878_Wp4W8.jpg",
   282923: "4d/c0/282923_WN3Ve.jpg",
   302050: "b6/25/302050_PMp77.jpg",
+  303246: "ce/c1/303246_lClcC.jpg",
   304270: "11/a0/304270_IS0IG.jpg",
-  353859: "e5/1e/353859_vvhVz.jpg",
+  308999: "6a/46/308999_k6K2y.jpg",
+  322229: "1f/97/322229_bsx4b.jpg",
+  326285: "3a/03/326285_rQAhA.jpg",
   380653: "ed/b3/380653_FaNAP.jpg",
   401778: "3a/78/401778_Nfqp0.jpg",
-  425417: "97/a2/425417_izmVF.jpg",
   435044: "bb/5c/435044_d4QNs.jpg",
+  456344: "98/5d/456344_WcdJ9.jpg",
   470936: "5a/98/470936_aAG01.jpg",
   485817: "43/ad/485817_NURv2.jpg",
-  490883: "ff/e9/490883_FQqCV.jpg",
   513940: "fe/0b/513940_6T7zn.jpg"
 };
