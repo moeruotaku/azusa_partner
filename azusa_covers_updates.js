709fbcbfce07629870c3e7a43bcf174413a1864c
moeruotaku <moeruotaku@gmail.com>
2026-04-21 20:10:53 +0800

    2026.04.21.84

--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.04.19.58
+// @version     2026.04.21.84
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,7 +12,6 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  32198: "https://img.azusa.wiki/i/2026/04/0a248c34-87b8-493f-b651-d7c8dd17a3f6.png",
   32223: "https://img.azusa.wiki/i/2026/04/a0de4549-4739-4174-a1b0-3487b7280b93.jpg",
   32240: "https://img.azusa.wiki/i/2026/04/b6b5608f-ba87-48ae-aa04-04d444e83ed2.jpg",
   32250: "https://img.azusa.wiki/i/2026/04/1f8d97c7-14fb-4703-900e-b31e8cd48236.jpg",
--- wall.js
+++ wall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_wall
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.04.19.58
+// @version     2026.04.21.84
 // @description add wall to azusa
 // @author      moeruotaku
 // @license     MIT
