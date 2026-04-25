e23a471076ea9b6cbdf5e4a806b315043e9f7da7
moeruotaku <moeruotaku@gmail.com>
2026-04-25 14:45:52 +0800

    2026.04.25.61

--- bgm_covers.js
+++ bgm_covers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_covers
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.04.24.72
+// @version     2026.04.25.61
 // @description bgm_covers
 // @author      moeruotaku
 // @license     MIT
--- bgm_scores.js
+++ bgm_scores.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_bgm_scores
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.04.24.72
+// @version     2026.04.25.61
 // @description bgm_scores
 // @author      moeruotaku
 // @license     MIT
