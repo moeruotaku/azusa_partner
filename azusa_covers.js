63acc23fa6bd86f8485412e58fb625b53b3edaf7
moeruotaku <moeruotaku@gmail.com>
2026-06-08 09:07:52 +0800

    2026.06.08.38

--- azusa_covers.js
+++ azusa_covers.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.06.04.61
+// @version     2026.06.08.38
 // @description azusa_covers
 // @author      moeruotaku
 // @license     MIT
@@ -1563,7 +1563,6 @@ const azusa_covers = {
   17998: "https://img.azusa.wiki/images/2023/08/15/0019cb47752587fabe9.jpg",
   17999: "https://img.azusa.wiki/images/2023/08/15/Wyvern_Girl_01.png",
   18003: "https://img.azusa.wiki/images/2023/08/15/0012a85a44a40d0464b.jpg",
-  18010: "https://img.gamegamept.com/upload/2023-6-6/4312606-shengnu-zhi-ge-heroine-anthem-ii-the-angel-of-sarem-windows-fron.jpg",
   18011: "https://img.azusa.wiki/images/2023/08/15/EVANGELION-Millennials2-1-5b17440c312b77d5.jpg",
   18015: "https://img.azusa.wiki/images/2023/08/15/137fd5119a6f73965.png",
   18016: "https://www.z4a.net/images/2023/08/15/img01.jpg",
@@ -3520,5 +3519,13 @@ const azusa_covers = {
   32943: "https://lain.bgm.tv/r/400/pic/cover/l/0a/2a/402640_l6Rr0.jpg",
   32950: "https://img.azusa.wiki/i/2026/06/6cdb88da-b315-4e72-a373-26917cd913f5.jpg",
   32958: "https://lain.bgm.tv/r/400/pic/cover/l/aa/a3/426810_GrW00.jpg",
-  32960: "https://img.azusa.wiki/i/2026/06/9418e165-de19-4f7a-94f0-b0c1ac5ab466.jpg"
+  32960: "https://img.azusa.wiki/i/2026/06/9418e165-de19-4f7a-94f0-b0c1ac5ab466.jpg",
+  32966: "https://img.azusa.wiki/i/2026/06/e5c5ad4f-d029-4179-aa7e-3d51a48cdc03.jpg",
+  32975: "https://lain.bgm.tv/r/400/pic/cover/l/55/f5/120547_jp.jpg",
+  32979: "https://img.azusa.wiki/i/2026/06/d9d14900-c3d5-494f-845a-09f99152cbe8.jpg",
+  32984: "https://img.azusa.wiki/i/2026/06/7897fd5c-8ad6-4226-aa25-2e4e07cf9de5.jpg",
+  32987: "https://img.azusa.wiki/i/2026/06/3d81b8ce-e4e3-4bc1-b027-947bd1e39904.jpg",
+  32989: "https://www.heroineanthem.com/vocal_version/zh-CN/images/banner5.jpg",
+  32990: "https://www.heroineanthem.com/vocal_version/zh-CN/images/banner2.jpg",
+  32993: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1373320/header.jpg?t=1657204091"
 };
--- azusa_covers_updates.js
+++ azusa_covers_updates.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        azusa_partner_library_azusa_covers_updates
 // @namespace   https://greasyfork.org/users/1396048-moeruotaku
-// @version     2026.06.04.61
+// @version     2026.06.08.38
 // @description azusa_covers_updates
 // @author      moeruotaku
 // @license     MIT
@@ -12,16 +12,19 @@
 // ==/UserScript==
 
 const azusa_covers = {
-  32817: "https://img.azusa.wiki/i/2026/05/c86f096a-bb96-43a3-8924-a7495f110daf.jpg",
-  32839: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2011240/d6b4368eac8ee74d7ebb4fc82e2c6c4d6c9b3fd5/header.jpg?t=1749983127",
-  32867: "https://img.azusa.wiki/i/2026/05/0d135d29-ad88-43d2-80fb-09f71b34fe83.jpg",
-  32871: "https://img.azusa.wiki/i/2026/05/3c56854a-b2de-4518-b6dd-62a09dc26a78.jpg",
-  32875: "https://img.azusa.wiki/i/2026/05/58bcde61-8298-44ae-9767-2cca13ba0f47.png",
   32935: "https://lain.bgm.tv/r/400/pic/cover/l/23/9e/287053_0bDpr.jpg",
   32941: "https://lain.bgm.tv/r/400/pic/cover/l/26/1a/405383_f9zD8.jpg",
   32942: "https://lain.bgm.tv/r/400/pic/cover/l/3d/eb/335877_nGcMh.jpg",
   32943: "https://lain.bgm.tv/r/400/pic/cover/l/0a/2a/402640_l6Rr0.jpg",
   32950: "https://img.azusa.wiki/i/2026/06/6cdb88da-b315-4e72-a373-26917cd913f5.jpg",
   32958: "https://lain.bgm.tv/r/400/pic/cover/l/aa/a3/426810_GrW00.jpg",
-  32960: "https://img.azusa.wiki/i/2026/06/9418e165-de19-4f7a-94f0-b0c1ac5ab466.jpg"
+  32960: "https://img.azusa.wiki/i/2026/06/9418e165-de19-4f7a-94f0-b0c1ac5ab466.jpg",
+  32966: "https://img.azusa.wiki/i/2026/06/e5c5ad4f-d029-4179-aa7e-3d51a48cdc03.jpg",
+  32975: "https://lain.bgm.tv/r/400/pic/cover/l/55/f5/120547_jp.jpg",
+  32979: "https://img.azusa.wiki/i/2026/06/d9d14900-c3d5-494f-845a-09f99152cbe8.jpg",
+  32984: "https://img.azusa.wiki/i/2026/06/7897fd5c-8ad6-4226-aa25-2e4e07cf9de5.jpg",
+  32987: "https://img.azusa.wiki/i/2026/06/3d81b8ce-e4e3-4bc1-b027-947bd1e39904.jpg",
+  32989: "https://www.heroineanthem.com/vocal_version/zh-CN/images/banner5.jpg",
+  32990: "https://www.heroineanthem.com/vocal_version/zh-CN/images/banner2.jpg",
+  32993: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1373320/header.jpg?t=1657204091"
 };
